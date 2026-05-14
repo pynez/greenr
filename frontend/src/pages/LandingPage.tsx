e280b0760f25d581568886ced3a8d8d14998e256
pynez <victorpynejr@outlook.com>
2026-05-14 00:35:13 -0500

    Fix TS discrepancy, build fail :(

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -142,7 +142,7 @@ export default function LandingPage() {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { duration: dur(0.5), ease: "easeOut" },
+      transition: { duration: dur(0.5), ease: "easeOut" as const },
     },
   };
 
